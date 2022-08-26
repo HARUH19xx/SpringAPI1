5a0f04d72bb0cb4ebd0a2af71c7ec3fc4a227fc0
H.Hokari <hookhull@gmail.com>
2022-08-26 15:16:27 +0900

    first commit

---
+++ src/main/kotlin/com/example/SpringAPI1/SpringApi1Application.kt
@@ -0,0 +1,11 @@
+package com.example.SpringAPI1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringApi1Application
+
+fun main(args:Array<String>) {
+	runApplication<SpringApi1Application>(*args)
+}
